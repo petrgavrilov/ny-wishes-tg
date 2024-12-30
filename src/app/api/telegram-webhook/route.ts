08056bc7e19bc77e83ea1abffcc761f31a440d02
petrgavrilov <petter426@gmail.com>
2024-12-30 22:01:29 +0100

    Reconfigure webhook

--- src/app/api/telegram-webhook/route.ts
+++ src/app/api/telegram-webhook/route.ts
@@ -4,4 +4,4 @@ export const fetchCache = "force-no-store";
 import { bot } from "@/bot/bot";
 import { webhookCallback } from "grammy";
 
-export const POST = webhookCallback(bot, "next-js");
+export const POST = webhookCallback(bot, "std/http");
