398bf332c6d9c35637e00e5cd7e98e99782fdda8
petrgavrilov <petter426@gmail.com>
2025-01-01 05:57:15 +0100

    send cards pictures

--- src/bot/handlers/send-wishes.ts
+++ src/bot/handlers/send-wishes.ts
@@ -1,15 +1,49 @@
 import { Bot } from "grammy";
 import { bot } from "../bot";
 import { MyContext } from "../types";
+import { wishes } from "@/data/wishes";
+import { WEBAPP_URL } from "../start";
+import { InputMediaPhoto } from "grammy/types";
+
+const MAX_PHOTOS = 10;
+
+function createChunks<T>(array: T[], size: number): T[][] {
+  return array.reduce<T[][]>((acc: T[][], _, index) => {
+    if (index % size === 0) {
+      acc.push(array.slice(index, index + size));
+    }
+    return acc;
+  }, []);
+}
+
+async function sendCards(chatId: string, wishesIds: string[]) {
+  const userWishes = wishes.filter((wish) => wishesIds.includes(wish.id));
+  const wishesText = userWishes
+    .map((wish) => `${wish.emoji} ${wish.descriptionRus}`)
+    .join("\n");
+  const imagesUrls = userWishes.map(
+    (wish) => `${WEBAPP_URL}/social-cards/${wish.id}.png`
+  );
+  const imagesUrlsChunks = createChunks(imagesUrls, MAX_PHOTOS);
+
+  await bot.api.sendMessage(chatId, `Твои желания:\n${wishesText}`);
+
+  for (const imagesUrlsChunk of imagesUrlsChunks) {
+    const media = imagesUrlsChunk.map<InputMediaPhoto>((url) => ({
+      type: "photo",
+      media: url,
+    }));
+    await bot.api.sendMediaGroup(chatId, media);
+  }
+}
 
 export async function sendWishes(chatId: string, wishesIds: string[]) {
-  await bot.api.sendMessage(chatId, wishesIds.join("\n"));
+  await sendCards(chatId, wishesIds);
 }
 
 export function setupWishes(bot: Bot<MyContext>) {
   bot.command("wishes", async (ctx) => {
-    console.log(`Wishes command received from ${ctx.chat?.id}`);
     const wishesIds = ctx.session.wishesIds;
-    await ctx.reply(`Your wishes: ${wishesIds.join(", ")}`);
+    await sendCards(ctx.chat.id.toString(), wishesIds);
   });
 }
